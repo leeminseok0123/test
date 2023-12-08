ea51e4463fd1fc4cca492e3ffae42b8b7cb12bc1
kevin <kevin.lee@teambooster.co.kr>
2023-12-09 01:12:04 +0900

    4번쨰 커밋

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,9 +1,16 @@
+# -*- coding: utf-8 -*-
+#
 import datetime
 
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, send_file
 from bs4 import BeautifulSoup
 import requests
 import xml.etree.ElementTree as ET
+from konlpy.tag import Okt
+from matplotlib import pyplot as plt
+from io import BytesIO
+
+
 
 def getToDay():
     return datetime.datetime.today().strftime("%Y%m%d")
@@ -43,6 +50,11 @@ def process_bloginfo_request():
     # HTTP POST 요청에서 'aaa' 파라미터를 받아옵니다.
     keyword = request.form.get('keyword', '')
     print(f"keyword: {keyword}")
+
+    keyword = "개인회생"
+
+    okt = Okt()
+    INCLUDE = ["Noun", "URL", "Alpha", "Number"]
 
     headers = {'User-Agent': (
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36')}
@@ -130,7 +142,159 @@ def process_bloginfo_request():
         total_len = len(contents)
         img_len = len(images)
 
-        ret_list.append([idx_x+1, post_url, uid, logNo, total_len, img_len, title, pub_date])
+        keyword_nouns = okt.pos(keyword)
+        print("keyword_nouns", keyword_nouns)
+
+        nouns = okt.pos(contents)
+
+        # 각 키워드가 contents 안에 몇 번 반복되었는지 확인하고 합산
+        num_of_keyword = 0
+        count_with_spaces = 0
+        count_without_spaces = 0
+
+        # 키워드가 띄어쓰기를 포함하는 경우 처리
+        if " " in keyword:
+            count_with_spaces = contents.count(keyword)
+            num_of_keyword += count_with_spaces
+            print(f"num_of_keyword {num_of_keyword}")
+        # 띄어쓰기를 제외한 키워드 생성
+        keyword_without_spaces = keyword.replace(" ", "")
+        # 띄어쓰기를 제외한 키워드 처리
+        count_without_spaces = contents.count(keyword_without_spaces)
+        num_of_keyword += count_without_spaces
+        print(f"count_without_spaces {count_without_spaces}")
+
+        print(f'키워드 "{keyword}"가 본문 안에 합쳐서 {num_of_keyword}번 반복됐습니다.')
+
+        word_list = ["했다", "겠다", "했었다", "였다", "하였다", "과거", "했습니다", "였습니다",
+                     "경험", "기억", "추억", "했던", "였던", "지난", "시절", "었다", "갔다", "갔었다", "았다"
+                                                                                  "이예요", "았어요", "했어요", "었어요", "어요",
+                     "있어요", "되어요", "습니다", "웠어요", "보았어요"]
+
+        # 결과를 저장할 딕셔너리를 초기화합니다.
+        word_frequency = {word: 0 for word in word_list}
+
+        # 문자열을 공백을 기준으로 단어로 분리합니다.
+        content_words = contents.split()
+        print(f"len(content_words): {len(content_words)}")
+        # 단어 리스트에 있는 각 단어를 문자열에서 찾고 빈도수를 계산합니다.
+        for idxx, content_word in enumerate(content_words):
+            print(idxx, content_word)
+            for word in word_list:
+                if word in content_word:
+                    word_frequency[word] += 1
+                    total_frequency += 1  # 모든 단어의 빈도수를 누적합니다.
+        # 결과 출력
+        for word, frequency in word_frequency.items():
+            print(f"{word}: {frequency}번")
+
+        print(f"총 빈도수: {total_frequency}번")
+
+        words = {}
+
+        str_temp = f"{idx_x + 1}위 {uid} ({user_info}) / {post_day}"
+        # legend.append(str_temp)
+        # legend.append(uid)
+
+        # INCLUDE = ["Noun", "URL", "Alpha", "Number"]
+        for n in nouns:
+            # 단어가 아니거나 2글자 이하인경우 제외
+            if n[1] not in INCLUDE or len(n[0]) < 2:
+                continue
+            if words.get(n[0]) is None:
+                words[n[0]] = 1
+            else:
+                words[n[0]] = words[n[0]] + 1
+            print(f"words: {words}")
+
+        print(f"words: {words}")
+
+        sorted_dict = sorted(words.items(), key=lambda item: item[1], reverse=True)
+        final_sorted_dict.extend(sorted_dict[:10])
+
+        sum_of_top = sum([len(w) * c for w, c in sorted_dict[:10]])
+        per = sum_of_top / total_len * 100
+
+        print(f"전체 글자수: {total_len} 상위단어: {sorted_dict[:10]} 상위단어비중: {per:.3f} 이미지갯수: {img_len}")
+        strtemp2 = f"글자수: {total_len} 이미지: {img_len} 키워드: {keyword}({num_of_keyword})=({count_with_spaces})+({count_without_spaces})"
+
+        str_temp3 = ""
+        # sorted_dict를 순회하면서 검색어와 일치하는 단어가 있는 경우 빈도수 출력
+        for word, count in sorted_dict:
+            print(f"word: {word} count: {count} keyword_nouns: {keyword_nouns}")
+
+            if any(word1 == word for word1, _ in keyword_nouns):
+                print(f'"{word}"는 keyword_nouns 안에 있습니다.')
+
+                print(f'검색어에 해당하는 단어: {word}, 빈도수: {count}')
+                str_temp3 += f'{word}({count}회) '
+
+        total_frequency = "{:2d}".format(total_frequency)
+        avg_visitor = "{:5d}".format(avg_visitor)
+        legend.append("DIA:" + str(total_frequency) + " 일" + str(
+            avg_visitor) + "명 / " + str_temp + " / " + strtemp2 + " " + str_temp3)
+
+        add_info = f"글자수: {total_len}자 이상\n"
+        add_info += f"키워드: {keyword} 본문에 {num_of_keyword}번 이상 포함되게 작성\n"
+        add_info += f"글자수: {total_len}자 이상\n"
+        add_info += f"글자수: {total_len}자 이상\n"
+
+        xy = [c for w, c in sorted_dict[:10]]
+        word = [w for w, c in sorted_dict[:10]]
+        for e, (x, w) in enumerate(zip(xy, word)):
+            if idx_x == 0:
+                plt.text(e, x, w, size=10, color="red", weight="bold", verticalalignment='bottom')
+            else:
+                plt.text(e, x, w, size=9, color="black", weight="normal", verticalalignment='bottom')
+            plt.plot(xy, "--o")
+
+    # 첫 번째 요소를 기준으로 반복 횟수를 더함
+    count_dict = {}
+    for item in final_sorted_dict:
+        key, count = item
+        if key in count_dict:
+            count_dict[key] += count
+        else:
+            count_dict[key] = count
+
+    # 반복 횟수를 기준으로 내림차순으로 정렬
+    sorted_count = sorted(count_dict.items(), key=lambda x: x[1], reverse=True)
+
+    # 상위 10개 요소 출력
+    top_10 = sorted_count[:10]
+    print(top_10)
+
+    for item in top_10:
+        print(item)
+
+    output_string = ', '.join([item[0] for item in top_10])
+
+    # 생성된 문자열 출력
+    print("참고 키워드", output_string)
+    #
+    # title_font = {
+    #     'fontsize': 20,
+    #     'color': "blue",
+    #     'fontweight': "bold",
+    #     'fontstyle': "italic"
+    # }
+    #
+    # plt.title(f"검색어: {keyword}", fontdict=title_font)
+    # plt.suptitle(output_string, fontsize=12)  # 부제목의 크기를 12로 설정
+    #
+    # plt.xlabel("키워드 종류")
+    # plt.ylabel("키워드 수")
+    # plt.legend(legend)
+    #
+    # img = BytesIO()
+    # plt.savefig(img, format='png', dpi=200)
+    # img.seek(0)
+    #
+    # return send_file(img, mimetype='image/png')
+
+    ret_list.append([idx_x+1, post_url, uid, logNo, total_len, img_len, title, pub_date, output_string])
+
+    print(f"output_string: {output_string}")
 
     # JSON 형식으로 응답합니다.
     response_data = {'keyword': keyword, 'data': ret_list}
